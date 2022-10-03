4ef955b6ae7d4b367ffcc7d4159489cc56f9ee9f
Nowshad Hasan <nowshadapu@gmail.com>
2022-10-03 22:05:59 +0600

    Initialize a basic java project

---
+++ src/PotatoMaker.java
@@ -0,0 +1,9 @@
+/**
+ * @author Nowshad Hasan
+ * @since 3/10/22 10:03 pm
+ */
+public class PotatoMaker {
+    void makePotato() {
+        System.out.println("Hi! Let's make some solid potatoes!");
+    }
+}
