a788f4d6fb06366dd1104ff015178f32361c2bb9
Nowshad Hasan <nowshadapu@gmail.com>
2022-10-06 10:14:14 +0600

    Update build.gradle file and refactor code

--- src/PotatoMaker.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Nowshad Hasan
- * @since 3/10/22 10:03 pm
- */
-public class PotatoMaker {
-    void makePotato() {
-        System.out.println("Hi! Let's make some solid potatoes!");
-    }
-}
---
+++ src/solid/potato/PotatoMaker.java
@@ -0,0 +1,11 @@
+package solid.potato;
+
+/**
+ * @author Nowshad Hasan
+ * @since 3/10/22 10:03 pm
+ */
+public class PotatoMaker {
+    public static void makePotato() {
+        System.out.println("Let's make some solid potatoes!");
+    }
+}
